5c7b99f8e7c46e37283d4a02618344224353806a
Nafiztonmoy <129676456+Nafiztonmoy@users.noreply.github.com>
2024-02-02 19:42:24 +0600

    first commit

---
+++ lib/NotificationPage.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class NotificationPage extends StatelessWidget {
+  const NotificationPage({
+    Key? key,
+    required this.payload,
+  }) : super(key: key);
+
+  final String payload;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Notification',
+          style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.w600),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.deepPurple,
+        actions: [
+          IconButton(
+            onPressed: () => {},
+            icon: Icon(Icons.menu, color: Colors.white),
+          ),
+        ],
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.deepPurple, Colors.purple],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Center(
+          child: Text(
+            payload,
+            style: TextStyle(fontSize: 30, color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+}
